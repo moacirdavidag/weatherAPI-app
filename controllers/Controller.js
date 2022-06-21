b487f77537be6310b13e9e652bb142ece47a72ee
moacirdavidag <moacirdavidag@gmail.com>
2022-06-21 17:10:38 -0300

    Adicionada view para erro

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -1,27 +1,31 @@
 
 const HOME = ((req, res) => {
     let city = "Sao_Paulo";
-    if(req.query.cidade) {
-        city = (req.query.cidade).replace("+", "_").normalize('NFD').replace(/[\u0300-\u036f]/g, "");   
+    if (req.query.cidade) {
+        city = (req.query.cidade).replace("+", "_").normalize('NFD').replace(/[\u0300-\u036f]/g, "");
     }
     const options = {
         method: "GET",
         mode: 'cors',
         cache: 'default'
     }
-    fetch(`http://api.weatherapi.com/v1/current.json?key=${process.env.API_KEY}&q=${city}`, options)
-    .then(response => response.json())
-    .then(function(data) {
-        res.status(200).render('home', {
-            cidade: data.location.name,
-            pais: data.location.country,
-            descricao: data.current.condition.text,
-            icone: data.current.condition.icon,
-            temperatura: (data.current.temp_c).toFixed(0),
-            umidade: data.current.humidity,
-            atualizacao: (data.current.last_updated).slice(11)
-        })
-    });
-});
 
-module.exports = {HOME};
+    fetch(`http://api.weatherapi.com/v1/current.json?key=${process.env.API_KEY}&q=${city}&lang=pt`, options)
+        .then(response => response.json())
+        .then(function (data) {
+            res.status(200).render('home', {
+                cidade: data.location.name,
+                pais: data.location.country,
+                descricao: data.current.condition.text,
+                icone: data.current.condition.icon,
+                temperatura: (data.current.temp_c).toFixed(0),
+                umidade: data.current.humidity,
+                atualizacao: (data.current.last_updated).slice(11)
+            })
+        }).catch((e) => {
+            res.status(400).render('error');
+        });
+}
+);
+
+module.exports = { HOME };
