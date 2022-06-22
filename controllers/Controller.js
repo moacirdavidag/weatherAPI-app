21552332c4066bedcd734d2daa121aba77dfb74d
moacirdavidag <moacirdavidag@gmail.com>
2022-06-22 09:30:06 -0300

    Corrigindo arquivo do Controller

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -3,30 +3,14 @@ const HOME = ((req, res) => {
     let city = "Sao_Paulo";
     if (req.query.cidade) {
         city = (req.query.cidade).replace("+", "_").normalize('NFD').replace(/[\u0300-\u036f]/g, "");
-    }
+    } 
+    
     const options = {
         method: "GET",
         mode: 'cors',
         cache: 'default'
     }
-<<<<<<< HEAD
-=======
-    fetch(`http://api.weatherapi.com/v1/current.json?key=${process.env.API_KEY}&q=${city}&lang=pt`, options)
-    .then(response => response.json())
-    .then(function(data) {
-        res.status(200).render('home', {
-            cidade: data.location.name,
-            pais: data.location.country,
-            descricao: data.current.condition.text,
-            icone: data.current.condition.icon,
-            temperatura: (data.current.temp_c).toFixed(0),
-            umidade: data.current.humidity,
-            atualizacao: (data.current.last_updated).slice(11)
-        })
-    });
-});
->>>>>>> api
-
+  
     fetch(`http://api.weatherapi.com/v1/current.json?key=${process.env.API_KEY}&q=${city}&lang=pt`, options)
         .then(response => response.json())
         .then(function (data) {
